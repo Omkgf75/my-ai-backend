a9b39b06fdd808bb62c3d6b51bc834106f214210
Omkgf75 <omar777rs@gmail.com>
2026-04-11 06:20:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 const Groq = require('groq-sdk');
 
 const app = express();
-
+// السماح للتطبيق بالاتصال بالسيرفر
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
